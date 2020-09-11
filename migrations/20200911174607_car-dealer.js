3d04db785dc6748bbfe9df623dfd697605eab566
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-11 17:54:18 -0400

    Created and ran car-dealer migration

---
+++ migrations/20200911174607_car-dealer.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("car-dealer", table=>{
+      table.text("vin").primary();
+      table.text("make").notNullable();
+      table.text("model").notNullable();
+      table.decimal("mileage").notNullable();
+      table.text("transmission_type");
+      table.text("title_status");
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("car-dealer");
+};
