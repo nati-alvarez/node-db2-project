4ab8c59cdaad95825f4f3b43ccb451ac51807657
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-11 19:02:03 -0400

    Added car routes

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ server.get("/api", (req, res)=>{
     res.send("welcome to the api");
 })
 
+const carRoutes = require("./cars/carRoutes");
+
+server.use("/api/cars", carRoutes);
+
 server.listen(5000, ()=> console.log("Listening on port 5000"));
