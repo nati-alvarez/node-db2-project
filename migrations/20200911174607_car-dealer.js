21dcd79eb7da2e7d30d158bbadf66506e11a9af1
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-11 23:15:32 -0400

    Created new migration; make primary key not nullable

--- migrations/20200911174607_car-dealer.js
+++ migrations/20200911174607_car-dealer.js
@@ -11,5 +11,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("car-dealer");
+  return knex.schema.dropTableIfExists("car-dealer")
 };
