53f53bff667dbf2f1143bbb2b68e86c85070cd1a
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-11 22:08:13 -0400

    Added error handling middleware

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ const carRoutes = require("./cars/carRoutes");
 
 server.use("/api/cars", carRoutes);
 
+server.use((error, req, res, next)=>{
+    res.send(500).json({message: "A server error occurred", error: error});
+})
+
 server.listen(5000, ()=> console.log("Listening on port 5000"));
