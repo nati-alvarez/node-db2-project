567ba1630a3d0e83d146111932c6418f1e0e502a
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-11 23:08:33 -0400

    Created POST /api/cars endpoint

--- cars/carRoutes.js
+++ cars/carRoutes.js
@@ -10,6 +10,18 @@ router.get("/", (req, res, next)=>{
     })
 })
 
+router.post("/", (req, res, next)=>{
+    const {vin, make, model, mileage, transmission_type, title_status} = req.body;
+    if(!vin || !make || !model || !mileage) return res.status(400).json({message: "Required fields not given"});
+    db("car-dealer")
+    .insert({vin, make, model, mileage, transmission_type, title_status})
+    .then(car=>{
+        res.status(201).json(req.body);
+    }).catch(err=>{
+        next(err);
+    })
+})
+
 router.get('/:vin', (req, res, next)=>{
     db("car-dealer")
     .where({vin: req.params.vin})
