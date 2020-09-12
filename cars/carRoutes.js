ca8db5ed5ebb4d312789aa4cb7ceecbf8c2f46a2
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-12 01:27:28 -0400

    Added 404 response to GET /api/cars/:vin endpoin in case car wasnt found

--- cars/carRoutes.js
+++ cars/carRoutes.js
@@ -26,6 +26,8 @@ router.get('/:vin', (req, res, next)=>{
     db("car-dealer")
     .where({vin: req.params.vin})
     .then(car=>{
+        car = car[0];
+        if(!car) return res.status(404).json({message: "Car not found"});
         res.status(200).json(car);
     }).catch(err=>{
         next(err);
